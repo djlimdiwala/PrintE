a0ba9adde084c19a73e179bf0660e48686a357fd
djlimdiwala <dhaval.limdiwala@gmail.com>
2018-03-03 02:58:38 +0530

    register and login working

---
+++ app/src/main/java/com/example/lkh/printe/firebase_id_service.java
@@ -0,0 +1,25 @@
+package com.example.lkh.printe;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by DJL on 3/3/2018.
+ */
+
+public class firebase_id_service extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("MyfirebaseToken", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+}
