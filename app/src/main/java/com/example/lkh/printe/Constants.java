f9333f8855d1f0bc30e0c0f4bb2cfbaaa683ed86
djlimdiwala <dhaval.limdiwala@gmail.com>
2018-03-23 18:37:55 +0530

    register and login working

---
+++ app/src/main/java/com/example/lkh/printe/Constants.java
@@ -0,0 +1,10 @@
+package com.example.lkh.printe;
+
+/**
+ * Created by DJL on 3/22/2018.
+ */
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
